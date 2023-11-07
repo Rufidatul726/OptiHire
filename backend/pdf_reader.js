2a6592824ff32f4f15b27dbffaa53d16b056b437
Muktadul Islam <76801029+MuktadulIslam@users.noreply.github.com>
2023-11-07 16:25:04 +0600

    ADD & UPDATE:  add skill reader function and update in contact reader function

--- backend/pdf_reader.js
+++ backend/pdf_reader.js
@@ -4,16 +4,37 @@ const natural = require('natural');
 async function getContactInfo(textDataArray) {
     let length = textDataArray.length
     let  contactInfo = [];
+    let foundContacts = false;
 
     for(let i=0;i<length  && !(textDataArray[i] == 'Top Skills' || textDataArray[i] == 'Languages' || textDataArray[i] == 'Certifications' || textDataArray[i] == 'Honors-Awards' || textDataArray[i] == 'Experience' || textDataArray[i] == 'Education') ; i++) {
-        contactInfo.push(textDataArray[i])
+        if(textDataArray[i] == 'Contact') foundContacts = true;
+
+        if(foundContacts) contactInfo.push(textDataArray[i])
     }
 
     // Removing extra infomation
     contactInfo.shift();
     contactInfo.pop()
-    return contactInfo;
     
+    return contactInfo;
+}
+
+
+async function getSkillsInfo(textDataArray) {
+    let length = textDataArray.length
+    let  skillsInfo = [];
+    let foundSkills = false;
+
+    for(let i=0;i<length  && !(textDataArray[i] == 'Languages' || textDataArray[i] == 'Certifications' || textDataArray[i] == 'Honors-Awards' || textDataArray[i] == 'Experience' || textDataArray[i] == 'Education') ; i++) {
+        if(textDataArray[i] == 'Top Skills') foundSkills = true;
+
+        if(foundSkills) skillsInfo.push(textDataArray[i])
+    }
+
+    // Removing extra infomation
+    skillsInfo.pop()
+
+    return skillsInfo;
 }
 
 
@@ -22,6 +43,9 @@ async function getInformationFromPDF(pdfBuffer) {
     // console.log(textDataArray,"\n\n")
     const contactInfo = await getContactInfo(textDataArray)
     console.log(contactInfo)
+
+    const skillsInfo = await getSkillsInfo(textDataArray)
+    console.log(skillsInfo)
 }
 
 module.exports = { 
